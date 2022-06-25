8dc289ac221ebc34327df78d963abefc30b9ace8
GetmanDima <dima.getman12112001@gmail.com>
2022-06-25 21:19:07 +0300

    Create badic Application API

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const authRouter = require("./authRouter");
+const applicationRouter = require("./applicationRouter");
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
+router.use("/applications", applicationRouter);
 
 module.exports = router;
