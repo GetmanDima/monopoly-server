c2d68d9bd6fbdb928b0e052a88eb6ea1fbf1f837
GetmanDima <dima.getman12112001@gmail.com>
2022-06-25 20:57:21 +0300

    Create Application model

--- models/Application.js
+++ models/Application.js
@@ -1,5 +1,39 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 
-const Application = new Schema({});
+const Application = new Schema({
+  actionTime: {
+    type: Number,
+    required: true,
+  },
+  userCount: {
+    type: Number,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  },
+  creator: {
+    type: {
+      _id: {
+        type: Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      nickname: String,
+      rating: Number,
+    },
+  },
+  board: {
+    type: {
+      _id: {
+        type: Types.ObjectId,
+        ref: "Board",
+        required: true,
+      },
+      name: String,
+    },
+  },
+});
 
 module.exports = model("Application", Application);
--- models/Board.js
+++ models/Board.js
@@ -1,5 +1,7 @@
 const { Schema, model, Types } = require("mongoose");
 
-const Board = new Schema({});
+const Board = new Schema({
+  name: String,
+});
 
 module.exports = model("Board", Board);
